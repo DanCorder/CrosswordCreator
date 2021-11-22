4d81662aae5138f91e00c90876e5445a9928bf7d
Dan Corder <dev@dancorder.com>
2021-11-22 17:52:12 +0000

    feat: Add search for matching words

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+let wordListDownloaded = false;
+let wordList = {};
+
+fetch('assets/js/processedWordList.json')
+    .then(response => response.json())
+    .then(data => {
+        wordList = data;
+        wordListDownloaded = true;
+    });
+
+document.getElementById('findMatchingWordsButton').onclick = function(ev) {
+    if (!wordListDownloaded) {
+        alert('Word list is still downloading, please try again or refresh the page');
+        return;
+    }
+    const input = document.getElementById('findMatchingWordsInput');
+    const output = document.getElementById('findMatchingWordsOutput');
+    const matches = findMatchingWords(input.value);
+    if (matches.length === 0) {
+        output.innerHTML = "No matches found";
+    } else {
+        output.innerHTML = matches.join('<br/>');
+    }
+};
+
+function findMatchingWords(pattern) {
+    const wordsOfSameLength = wordList[pattern.length];
+    let matches = [];
+    const regex = new RegExp(pattern.replace('.', '\\w'));
+
+    Object.keys(wordsOfSameLength).forEach(candidate => {
+        if (regex.test(candidate)) {
+            matches = matches.concat(wordsOfSameLength[candidate]);
+        }
+    });
+
+    return matches;
+}
--- index.md
+++ index.md
@@ -11,3 +11,10 @@ Tools for cryptic crossword creation - under construction
 * [Free thesaurus](https://www.thesaurus.com/)
 * [Different clue types](https://puzzling.stackexchange.com/questions/45984/cryptic-clue-guide)
 * [Clues for letters](https://en.wikipedia.org/wiki/Crossword_abbreviations)
+
+<div>
+    <h2>Find Matching Words</h2>
+    <p>Enter the pattern to match below. Use letters where you have them and '.' for empty spaces</p>
+    <input id="findMatchingWordsInput" /><button id="findMatchingWordsButton">Search</button>
+    <div id="findMatchingWordsOutput"></div>
+</div>
