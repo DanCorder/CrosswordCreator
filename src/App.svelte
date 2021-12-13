b3e722f76174c2615b5200e94719ddb5577b4cbc
Dan Corder <dev@dancorder.com>
2021-12-13 21:48:49 +0000

    feat: styling working

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,56 @@
-<script lang="ts">
-	export let name: string;
-</script>
+<div class='main'>
+    <div class="content-block">
+        <h1>Tools for cryptic crossword creation - under construction</h1>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<div>SASS is working?</div>
-</main>
+        <ul>
+            <li><a href="https://www.wordplays.com/anagrammer" target="_blank">Anagrams</a></li>
+            <li><a href="https://www.dictionary.com/" target="_blank">Free dictionary</a></li>
+            <li><a href="https://www.thesaurus.com/" target="_blank">Free thesaurus</a></li>
+            <li><a href="https://puzzling.stackexchange.com/questions/45984/cryptic-clue-guide|" target="_blank">Different clue types</a></li>
+            <li><a href="https://en.wikipedia.org/wiki/Crossword_abbreviations" target="_blank">Clues for letters</a></li>
+        </ul>
+    </div>
+
+    <div class="content-block">
+        <h2>Find Words That Fit</h2>
+        <p>Enter the pattern to match below. Use letters where you have them and '.' or space for empty spaces</p>
+        <input id="findMatchingWordsInput" /> <button id="findMatchingWordsButton">Search</button>
+        <div id="findMatchingWordsOutput"></div>
+    </div>
+
+    <div class="content-block">
+        <h2>Find Anagrams</h2>
+        <p>All single letters can be considered words in some way. To reduce the number of results we limit single letter words to just 'a' and 'I'.</p>
+        <p>Find up to 1000 results</p>
+        <input id="findAnagramsInput" /> <button id="findAnagramsButton">Search</button> Exclude these letters <input id="findAnagramsExclude" />
+        <div id="findAnagramsOutput"></div>
+    </div>
+
+    <div class="content-block">
+        <h2>Find Single Anagrams</h2>
+        <p>Find all single word anagrams within the input, not necessarily using all letters</p>
+        <input id="findSingleAnagramsInput" /> <button id="findSingleAnagramsButton">Search</button>
+        <div id="findSingleAnagramsOutput"></div>
+    </div>
+
+    <div class="content-block">
+        <a href="/CrosswordCreator/credits">Credits/Copyright</a>
+    </div>
+</div>
 
 <style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+    .content-block {
+        background-color: $text-background-colour;
+        margin: $content-margin calc($content-margin / 2);
+        padding: 20px;
+        border-radius: 4px;
+        box-shadow: 4px 4px 2px 2px rgba(0,0,0,0.1);
+    }
 
-		div {
-			background-color: green;
-		}
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    a {
+        color: $darker-colour;
+        &:visited {
+            color: $medium-colour;
+        }
+    }
 </style>
